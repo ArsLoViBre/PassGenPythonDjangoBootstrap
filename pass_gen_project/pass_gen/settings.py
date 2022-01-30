5313712f431e908ba511396390bf5f0b72fe8603
Ars T <32305529+ArsLoViBre@users.noreply.github.com>
2022-01-30 17:57:39 +0100

    Update settings.py

--- pass_gen_project/pass_gen/settings.py
+++ pass_gen_project/pass_gen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from DjangoSecretKey import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!xk7v@lgj7gw5z$86*vmkf@0=&4=-etz$_2uwl8e$62@f36%ff'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
